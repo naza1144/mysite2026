9ca9ec273f3000ee6042fd36d6648c39eb290226
naza1144-dev-linux <knnbos77@gmail.com>
2026-06-18 15:28:23 +0700

    redme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # mysite2026
 web-project for learning Django in Web Dev 2026/1
+
+# mysite2026
+## คำสั่งที่ใช้
+    uv run manage.py runserver [IP_ADDRESS]:80
+    
+## http methods
+    -post
+    -put
+    -get
+    -delete
+
+## status code
+    -200 ok
+    -201 created
+    -400 bad request
+    -404 not found
+    -405 method not allowed
+    -500 internal server error
---
+++ mysite2026/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for mysite2026 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def info(request):
+    ip_addres = request.META['REMOTE_ADDR']
+    res = f"<h1>you Ip address is : {ip_addres}</h1>"
+    for name, value in request.META.items():
+        res += f"<p>{name}: {value}</p>"
+    return HttpResponse(res)
+
+
+
+
+
+urlpatterns = [
+    path('info/', info),
+    path('admin/', admin.site.urls),
+]
